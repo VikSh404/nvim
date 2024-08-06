0de8f6389d2bd4ac81a60b998f0662c5ec903a01
Viktor Shevchenko <viktor.shevchenko@smartstream-stp.com>
2024-08-06 22:17:58 +0200

    nvim config
    nvim config

---
+++ init.lua
@@ -0,0 +1,11 @@
+--vim.cmd("let mapleader = ' '")
+require("preferences")
+require("keymaps")
+require("config.lazy")
+
+
+
+--print("Hello from init.lua")
+--#vim.opt.colorscheme = "retrobox"
+--vim.cmd("colorscheme retrobox")
+vim.cmd("colorscheme nord")
